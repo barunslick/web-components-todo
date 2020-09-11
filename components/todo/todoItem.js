23f5b94fd57bdb7919b255ee77efe7ccc5ea4bb7
barunslick <barunpradhan987@gmail.com>
2020-09-11 14:38:35 +0545

    Change todoItem to lit-element

--- components/todo/todoItem.js
+++ components/todo/todoItem.js
@@ -1,4 +1,4 @@
-import { html, render } from '../../node_modules/lit-html/lit-html.js';
+import { LitElement, html, css } from 'lit-element';
 
 /**
  * Class for each item in Todo.
@@ -6,7 +6,34 @@ import { html, render } from '../../node_modules/lit-html/lit-html.js';
  * @class TodoItem
  * @extends {HTMLElement}
  */
-class TodoItem extends HTMLElement {
+class TodoItem extends LitElement {
+  static get styles() {
+    return css`
+      .todoItem {
+        display: flex;
+        align-items: center;
+        margin: 8px 0px;
+        padding: 4px 8px;
+        border: 1px solid #99b1bc;
+        border-radius: 8px;
+      }
+      p {
+        flex: 3;
+      }
+      .done {
+        color: gray;
+        text-decoration: line-through;
+        background: #ccdae0;
+      }
+      .todoName {
+        cursor: pointer;
+      }
+
+      #deleteTodo {
+        display: inline-block;
+      }
+    `;
+  }
   /**
    * Monitor complete attribute for changes.
    *
@@ -14,8 +41,10 @@ class TodoItem extends HTMLElement {
    * @static
    * @memberof TodoItem
    */
-  static get observedAttributes() {
-    return ['complete'];
+  static get properties() {
+    return {
+      complete: { type: String },
+    };
   }
 
   /**
@@ -25,87 +54,21 @@ class TodoItem extends HTMLElement {
   constructor() {
     super();
 
-    this.attachShadow({ mode: 'open' });
+    this.complete = 'false';
+  }
 
-    const template = html`
-      <style>
-        .todoItem {
-          display: flex;
-          align-items: center;
-          margin: 8px 0px;
-          padding: 4px 8px;
-          border: 1px solid #99b1bc;
-          border-radius: 8px;
-        }
-        p {
-          flex: 3;
-        }
-        .done {
-          color: gray;
-          text-decoration: line-through;
-          background: #ccdae0;
-        }
-        .todoName {
-          cursor: pointer;
-        }
-
-        #deleteTodo {
-          display: inline-block;
-        }
-      </style>
-
-      <div class="todoItem">
-        <p class="todoName"></p>
-        <custom-button id="deleteTodo"> - </custom-button>
+  render() {
+    return html`
+      <div class="todoItem ${this.complete === 'true' ? 'done' : ''}">
+        <p class="todoName" @click="${this.toggleComplete}">
+          ${this.getAttribute('name')}
+        </p>
+        <custom-button id="deleteTodo" @click="${this.deleteTodo}">
+          -
+        </custom-button>
       </div>
     `;
-
-    render(template, this.shadowRoot);
-
-    this.todoName = this.shadowRoot.querySelector('.todoName');
-
-    this.deleteTodoBtn = this.shadowRoot.querySelector('#deleteTodo');
   }
-
-  /**
-   * Changes class name when complete is toggled to represent if a todo is done/complete or not.
-   *
-   * @param {String} attrName
-   * @param {*} oldVal
-   * @param {*} newVal
-   * @memberof TodoItem
-   */
-  attributeChangedCallback(attrName, oldVal, newVal) {
-    if (attrName === 'complete') {
-      if (newVal === 'true') {
-        this.shadowRoot.querySelector('.todoItem').classList.add('done');
-      } else {
-        this.shadowRoot.querySelector('.todoItem').classList.remove('done');
-      }
-    }
-  }
-
-  /**
-   * Creates event listeners and sets todoName value after attached to Dom.
-   *
-   * @memberof TodoItem
-   */
-  connectedCallback() {
-    this.todoName.innerText = this.getAttribute('name');
-    this.todoName.addEventListener('click', this.toggleComplete);
-    this.deleteTodoBtn.addEventListener('click', this.deleteTodo);
-  }
-
-  /**
-   * Clears all event listeners upon unmounting from Dom.
-   *
-   * @memberof TodoItem
-   */
-  disconnectedCallback() {
-    this.todoName.addEventListener('click', this.toggleComplete);
-    this.deleteTodoBtn.removeEventListener('click', this.deleteTodo);
-  }
-
   /**
    * Toggles a todo as complete or not complete.
    *
